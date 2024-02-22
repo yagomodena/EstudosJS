5e3c90b1d6149eb74f09e5f985dbdbebd490c0bc
Yago Modena <modenayago@yahoo.com>
2024-02-21 21:51:30 -0300

    Commit com a primeira function

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 var area = document.getElementById('area')
 
-function entrar(){
+function entrar() {
     var nome = prompt("Digite seu nome");
+
+    if (nome === '' || nome === null) {
+        alert("Ops algo deu errado")
+        area.innerHTML = "Clique no botão para acessar..."
+    } else {
+        area.innerHTML = "Bem vindo " + nome;
+    }
 }
