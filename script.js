6902d4cadb0887f8d886d44abd26da3fb2e5aaf1
Yago Modena <modenayago@yahoo.com>
2024-02-21 21:56:35 -0300

    comando de outra function, botão de acessar e sair.

--- script.js
+++ script.js
@@ -7,6 +7,16 @@ function entrar() {
         alert("Ops algo deu errado")
         area.innerHTML = "Clique no botão para acessar..."
     } else {
-        area.innerHTML = "Bem vindo " + nome;
+        area.innerHTML = "Bem vindo " + nome + " ";
+
+        let botaoSair = document.createElement("button");
+        botaoSair.innerText = "Sair da conta";
+        botaoSair.onclick = sair;
+        area.appendChild(botaoSair);
     }
+}
+
+function sair(){
+    alert("Até mais!");
+    area.innerHTML = "Você saiu!"
 }
